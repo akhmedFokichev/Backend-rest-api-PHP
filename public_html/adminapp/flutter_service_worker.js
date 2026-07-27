e69a5c60c5a74b3b38be2b2fb9f02e0f95572df2
Akhmed <f89034909970@gmail.com>
2026-07-27 22:00:52 +0300

    fix

--- public_html/adminapp/flutter_service_worker.js
+++ public_html/adminapp/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "13f94f4ca309260380f341a63c90502e",
+const RESOURCES = {"flutter_bootstrap.js": "113fdc6971d5d1b3a7fd293d8e03cf2b",
 "version.json": "9f9b94200e97e61492e999629955767b",
 "index.html": "99d7838275ad52ee143c84f02a732d14",
 "/": "99d7838275ad52ee143c84f02a732d14",
-"main.dart.js": "bf1da4755c563a52ad7326b5df10af0c",
+"main.dart.js": "7f95b7901406c88966c81c1fb511cbca",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
